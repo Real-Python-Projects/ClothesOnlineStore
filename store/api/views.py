49ca1fd242a2d9926d511bcaa0ce9b7f13c7009e
Gateway47 <itsregalo047@gmail.com>
2021-04-08 12:58:45 +0300

    finished daraja C2B intergration

--- store/api/views.py
+++ store/api/views.py
@@ -6,7 +6,22 @@ from store.api.serializers import MpesaPaymentSerializer
 from store.models import MpesaPayment
 import json
 import requests
+from requests.auth import HTTPBasicAuth
 from store.mpesa_credentials import MpesaAccessToken,LipaNaMpesaPassword
+
+
+@api_view(['GET'])
+@csrf_exempt
+def api_register_urls(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    options = {"ShortCode": LipaNaMpesaPassword.test_c2b_short_code,
+               "ResponseType": "Completed",
+               "ConfirmationURL": "https://26305433cb94.ngrok.io/api/c2b/confirmation/",
+               "ValidationURL": "https://26305433cb94.ngrok.io/api/c2b/validation/"}
+    response = requests.post(api_url, json=options, headers=headers)
+    return Response(response.text)
 
 @api_view(['GET'])
 @csrf_exempt
@@ -15,47 +30,32 @@ def api_call_back(request):
 
 @api_view(['GET'])
 @csrf_exempt
-def api_register_urls(request):
-    access_token = MpesaAccessToken.validated_mpesa_access_token
-    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-    headers = {"Authorization":"Bearer %s" % access_token}
-    options = {"ShortCode": LipaNaMpesaPassword.business_short_code,
-               "ResponseType":"Completed",
-               "ConfirmationUrl":"https://5590a37a7745.ngrok.io/c2b/confirmation",
-               "ValidationUrl": "https://5590a37a7745.ngrok.io/c2b/validation",
-               }
-    response = requests.post(api_url, json=options, headers=headers)
-    return Response(response.text)
-
-@api_view(['GET'])
-@csrf_exempt
 def api_validation(request):
     context = {
-        "ResultCode":0,
-        "ResultDesc":"Accepted"
+        "ResultCode": 0,
+        "ResultDesc": "Accepted"
     }
     return Response(dict(context))
 
+@api_view(['GET'])
 @csrf_exempt
 def api_confirmation(request):
-    mpesa_body = request.body.decode('utf-8')
+    mpesa_body =request.body.decode('utf-8')
     mpesa_payment = json.loads(mpesa_body)
-    
-    payment = MpesaPayment (
-        first_name = mpesa_payment['FirstName'],
-        last_name = mpesa_payment['LastName'],
-        middle_name = mpesa_payment['MiddleName'],
-        description = mpesa_payment['TransID'],
-        phone_number = mpesa_payment['MSISDN'],
-        amount = mpesa_payment['TransAmount'],
-        reference = mpesa_payment['BillRefNumber'],
-        organization_balance = mpesa_payment['OrgAccountBalance'],
-        type = mpesa_payment['TransactionType']
+    payment = MpesaPayment(
+        first_name=mpesa_payment['FirstName'],
+        last_name=mpesa_payment['LastName'],
+        middle_name=mpesa_payment['MiddleName'],
+        description=mpesa_payment['TransID'],
+        phone_number=mpesa_payment['MSISDN'],
+        amount=mpesa_payment['TransAmount'],
+        reference=mpesa_payment['BillRefNumber'],
+        organization_balance=mpesa_payment['OrgAccountBalance'],
+        type=mpesa_payment['TransactionType'],
     )
     payment.save()
     context = {
-        "ResultCode":0,
-        "ResultDesc":"Accepted"
+        "ResultCode": 0,
+        "ResultDesc": "Accepted"
     }
-    
     return Response(dict(context))
